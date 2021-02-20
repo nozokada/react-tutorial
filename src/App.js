f107e56b9efb77e6d9301fc21685dd61196610ed
Nozomi Okada <nozomiokada@workfront.com>
2021-02-20 08:47:12 -0700

    Added Form List

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import {useState} from 'react';
 import {List} from './List';
+import {Form} from "./Form";
 
 function App() {
-    const [description, setDescription] = useState('クリック前の表示');
-
-    const changeDescription = () => {
-        setDescription('クリック後の表示です')
-    }
+    const [tab, setTab] = useState('list');
 
     return (
         <div>
-            {description}
-            <List title="取り扱い言語一覧"/>
-            <button onClick={changeDescription}>ボタン</button>
+            <header>
+                <ul>
+                    <li onClick={() => setTab('list')}>リスト</li>
+                    <li onClick={() => setTab('form')}>フォーム</li>
+                </ul>
+            </header>
+            <hr/>
+            {
+                tab === 'list' ? <List/> : <Form />
+            }
         </div>
     );
 }
